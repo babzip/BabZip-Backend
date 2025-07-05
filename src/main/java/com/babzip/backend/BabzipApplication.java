a7b9e4c18aef5ec5d18712139e4c7f5ee563992b
kon28289 <kon28289@naver.com>
2025-07-05 15:36:12 +0900

    inital commit

---
+++ src/main/java/com/babzip/backend/BabzipApplication.java
@@ -0,0 +1,13 @@
+package com.babzip.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabzipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabzipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/babzip/backend/BabzipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babzip.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabzipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
