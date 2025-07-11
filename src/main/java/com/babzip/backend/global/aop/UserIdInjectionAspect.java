cb0e374dd434804984c9f36c2710dc2dab9e8a27
kon28289 <kon28289@naver.com>
2025-07-11 20:08:25 +0900

    feat : Aop 추가

---
+++ src/main/java/com/babzip/backend/global/aop/UserIdInjectionAspect.java
@@ -0,0 +1,60 @@
+package com.babzip.backend.global.aop;
+
+import com.babzip.backend.global.jwt.JwtAuthentication;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Parameter;
+
+@Slf4j
+@Aspect
+@Component
+public class UserIdInjectionAspect {
+    @Pointcut("@annotation(com.babzip.backend.global.aop.AssignUserId) && (args(userId, ..) || args(.., userId))")
+    public void userIdAnnotatedMethod(Long userId) {
+    }
+
+    @Around(value = "userIdAnnotatedMethod(userId)", argNames = "joinPoint,userId")
+    public Object injectUserId(ProceedingJoinPoint joinPoint, Long userId) throws Throwable {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        log.info("UserId : {}", (Long) authentication.getPrincipal());
+
+        if (authentication instanceof JwtAuthentication) {
+            userId = (Long) authentication.getPrincipal();
+        } else if (authentication instanceof AnonymousAuthenticationToken) {
+            MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+            AssignUserId assignUserIdAnnotation = signature.getMethod().getAnnotation(AssignUserId.class);
+            if (assignUserIdAnnotation.required()) {
+                log.error("Null Authentication error {}", authentication.getClass());
+                throw new IllegalArgumentException("You must login to use this service.");
+            } else {
+                log.info("Anonymous Authentication, userId is null");
+                userId = null;
+            }
+        } else {
+            log.error("JwtAuthentication Type Casting Error {}", authentication.getClass());
+            throw new IllegalArgumentException("Only JWT-based authentication is supported for @AssignUserId annotation.");
+        }
+
+        Object[] args = joinPoint.getArgs();
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Parameter[] parameters = signature.getMethod().getParameters();
+
+        for (int i = 0; i < parameters.length; i++) {
+            if (parameters[i].getType() == Long.class && args[i] == null) {
+                args[i] = userId;
+                break;
+            }
+        }
+
+        return joinPoint.proceed(args);
+    }
+}
