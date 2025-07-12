3e64af8f07e2bda9106ce03ec66887876e6a3e85
kon28289 <kon28289@naver.com>
2025-07-12 13:53:29 +0900

    feat: 로그아웃 구현

--- src/main/java/com/babzip/backend/user/controller/UserController.java
+++ src/main/java/com/babzip/backend/user/controller/UserController.java
@@ -1,18 +1,36 @@
 package com.babzip.backend.user.controller;
 
+import com.babzip.backend.global.aop.AssignUserId;
+import com.babzip.backend.global.oauth.service.OAuth2UserPrincipal;
+import com.babzip.backend.global.response.ResponseBody;
+import com.babzip.backend.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
+@Slf4j
 public class UserController {
+
+    private final UserService userService;
 
     @GetMapping("/hello")
     @PreAuthorize(" isAuthenticated() and hasAuthority('USER')")
     public String hello(){
         return "Hello World";
+    }
+
+    @AssignUserId
+    @DeleteMapping("/logout")
+    @PreAuthorize(" isAuthenticated() and hasAuthority('USER')")
+    public ResponseEntity<ResponseBody<Void>> logout(Long userId){
+        userService.logout(userId);
+        return ResponseEntity.ok(null);
     }
 
 }
