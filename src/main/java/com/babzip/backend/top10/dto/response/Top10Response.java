1247d579967c237daa308221f4440c36252c0d3f
kon28289 <kon28289@naver.com>
2025-07-17 17:38:49 +0900

    feat: dto 추가

---
+++ src/main/java/com/babzip/backend/top10/dto/response/Top10Response.java
@@ -0,0 +1,17 @@
+package com.babzip.backend.top10.dto.response;
+
+import com.babzip.backend.top10.domain.Top10;
+
+public record Top10Response (
+        String restaurantName,
+        String address,
+        Long rank
+){
+    public static Top10Response toDto(Top10 top10) {
+        return new Top10Response(
+                top10.getRestaurantName(),
+                top10.getAddress(),
+                top10.getRank()
+        );
+    }
+}
