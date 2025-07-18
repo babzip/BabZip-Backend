028df03634a3836bd6b05caf8f2888c876cf09b5
kon28289 <kon28289@naver.com>
2025-07-18 19:40:40 +0900

    chore: Guestbook 도메인에 주소 필드 추가

--- src/main/java/com/babzip/backend/guestbook/entity/Guestbook.java
+++ src/main/java/com/babzip/backend/guestbook/entity/Guestbook.java
@@ -15,24 +15,26 @@ public class Guestbook extends BaseEntity {
     private Long id;
 
     private String restaurantName;
+    private String address;
     private String kakaoPlaceId;
     private String content;
-    private Integer rating;
+    private Long rating;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
     @Builder
-    public Guestbook(String restaurantName, User user, String kakaoPlaceId, String content, Integer rating) {
+    public Guestbook(String restaurantName, String address,User user, String kakaoPlaceId, String content, Long rating) {
         this.restaurantName = restaurantName;
+        this.address = address;
         this.user = user;
         this.kakaoPlaceId = kakaoPlaceId;
         this.content = content;
         this.rating = rating;
     }
 
-    public void updatePartial(String restaurantName,String kakaoPlaceId, String content, Integer rating) {
+    public void updatePartial(String restaurantName,String kakaoPlaceId, String content, Long rating) {
         if (restaurantName != null) this.restaurantName = restaurantName;
         if (kakaoPlaceId != null) this.kakaoPlaceId = kakaoPlaceId;
         if (content != null) this.content = content;
