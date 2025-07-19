3418569e6d5469c7b9dd0dc216755b933e8bd5a3
kon28289 <kon28289@naver.com>
2025-07-19 20:30:38 +0900

    feat: 임시로 로깅 코드 추가

--- src/main/java/com/babzip/backend/global/logging/GlobalExceptionHandler.java
+++ src/main/java/com/babzip/backend/global/logging/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.babzip.backend.global.logging;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,8 +12,8 @@ public class GlobalExceptionHandler {
 
     // 모든 예외 처리 (500 에러 포함)
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleAllExceptions(Exception ex) {
-        log.error("🔥 500 에러 발생! 예외 메시지: {}", ex.getMessage(), ex);
+    public ResponseEntity<Object> handleAllExceptions(Exception ex, HttpServletRequest request) {
+        log.error("🔥 [{} {}] 요청 중 예외 발생: {}", request.getMethod(), request.getRequestURI(), ex.getMessage(), ex);
         return ResponseEntity.internalServerError().body("서버 오류가 발생했습니다.");
     }
 }
